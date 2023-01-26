1d7541d95f4e10f5e4f9e4d2bd7be04400958e88
rodnoy <ksimagin@gmail.com>
2023-01-26 20:46:06 +0100

    add colliders

---
+++ K6-3D-PR063/Assets/Scripts/Collider/CollitionCheck.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CollitionCheck : MonoBehaviour
+{
+    public GameObject T_cube;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("<color=yellow>entered</color> the trigger");
+        ChangeCubeColor(Color.yellow);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("<color=green>is withing</color> the trigger");
+        ChangeCubeColor(GenerateRandomColor());
+    }
+
+
+    
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("<color=orange>exited</color> the trigger");
+        ChangeCubeColor(Color.red);
+    }
+
+    private void ChangeCubeColor(Color c_color)
+    {
+        T_cube.transform.GetComponent<MeshRenderer>().material.color = c_color;
+    }
+    private Color GenerateRandomColor()
+    {
+        Color background = new Color(
+        Random.Range(0f, 1f),
+        Random.Range(0f, 1f),
+        Random.Range(0f, 1f)
+        );
+        return background;
+    }
+}
